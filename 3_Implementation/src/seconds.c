c424e6a9e18cad1d8fc3b5134fca6676c0cd0bfe
Renganathan-S <94215831+Renganathan-S@users.noreply.github.com>
2021-12-02 15:22:28 +0530

    Update seconds.c

--- 3_Implementation/src/seconds.c
+++ 3_Implementation/src/seconds.c
@@ -4,3 +4,47 @@
  * @date 2021-12-02
  * @author Renganathan
  */ 
+void CalculateSeconds (bool time_display)
+{
+	switch (time_display){
+		case 0:
+				if (seconds/10 == 0 && seconds < 10){
+					WriteDisplays(0,1);
+					WriteDisplays(seconds,0);
+				}
+
+				else if (seconds%10 != 0){
+					WriteDisplays(seconds/10,1);
+					WriteDisplays(seconds%10,0);
+				}
+	
+				else if (seconds%10 == 0){
+					WriteDisplays(seconds/10,1);
+					WriteDisplays(0,0);
+				}
+		break;
+		
+		case 1:
+				if (seconds/10 == 0 && seconds < 10){
+					WriteDisplays(0,1);
+					_delay_ms(25);
+					WriteDisplays(seconds,0);
+					_delay_ms(25);
+				}
+
+				else if (seconds%10 != 0){
+					WriteDisplays(seconds/10,1);
+					_delay_ms(25);
+					WriteDisplays(seconds%10,0);
+					_delay_ms(25);
+				}
+				
+				else if (seconds%10 == 0){
+					WriteDisplays(seconds/10,1);
+					_delay_ms(25);
+					WriteDisplays(0,0);
+					_delay_ms(25);
+				}
+		break;
+	}
+}//~~ end of CalculateSeconds
