c45b8cda20406993dbd52af510108c193945e251
Renganathan-S <94215831+Renganathan-S@users.noreply.github.com>
2021-12-02 15:19:16 +0530

    Update minutes.c

--- 3_Implementation/src/minutes.c
+++ 3_Implementation/src/minutes.c
@@ -4,3 +4,47 @@
  * @date 2021-12-02
  * @author Renganathan
  */ 
+void CalculateMinutes (bool time_display)
+{
+	switch (time_display){
+		case 0:
+				if (minutes/10 == 0 && minutes < 10){
+					WriteDisplays(0,1);
+					WriteDisplays(minutes, 0);
+				}
+	
+				else if (minutes%10 == 0){
+					WriteDisplays(minutes/10, 1);
+					WriteDisplays(0,0);
+				}
+	
+				else if (minutes%10 != 0){
+					WriteDisplays(minutes/10, 1);
+					WriteDisplays(minutes%10,0);
+				}
+		break;
+		
+		case 1:
+				if (minutes/10 == 0 && minutes < 10){
+					WriteDisplays(0,1);
+					_delay_ms(25);
+					WriteDisplays(minutes, 0);
+					_delay_ms(25);
+				}
+				
+				else if (minutes%10 == 0){
+					WriteDisplays(minutes/10, 1);
+					_delay_ms(25);
+					WriteDisplays(0,0);
+					_delay_ms(25);
+				}
+				
+				else if (minutes%10 != 0){
+					WriteDisplays(minutes/10, 1);
+					_delay_ms(25);
+					WriteDisplays(minutes%10,0);
+					_delay_ms(25);
+				}
+		break;
+	}	
+}//~~ end of CalculateMinutes
